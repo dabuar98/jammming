7a16e3f0b7de8c95a8d82967a6e9b99c0f46fa3f
Daniel Bulla <dabuar98@gmail.com>
2024-08-28 18:36:24 -0500

    Add search icon and styling

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './SearchBar.css'
 
 function SearchBar () {
     const [search, setSearch] = useState("");
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`The form has been submitted with search word ${search}`)
+        alert(`The form has been submitted with search word: ${search}`)
     }
 
     return(
-        <form onSubmit={handleSubmit} >
+        <form onSubmit={handleSubmit} className="search-bar">
             <input value={search} onChange={({ target }) => setSearch(target.value)}></input>
-            <button type="submit">Search</button>
+            <button type="submit"><FontAwesomeIcon className="search-icon" icon={faSearch}/></button>
         </form>
     )
 }
