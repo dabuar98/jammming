91afd9438349b9253a8a2104105988ccc86812a6
Daniel Bulla <dabuar98@gmail.com>
2024-08-28 18:51:31 -0500

    Improve page layout and add style sheet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-// import Track from './Track'
+import React from 'react';
 import SearchBar from './SearchBar'
 import SearchResults from './SearchResults';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpotify } from '@fortawesome/free-brands-svg-icons'
+import './App.css'
 
 function App() {
   return (
-    <>
-      <SearchBar />    
-      {/* <Track  albumCover="../public/logo192.png" 
-              trackName="Sample Track" 
-              trackAuthor="Sample Author" 
-              trackYear="Sample Year" /> */}
+  
+      <div className="container">
 
-      <SearchResults />
-    </>
+        <header>
+            <h1>Jammming with <FontAwesomeIcon icon={faSpotify}/> Spotify</h1>
+            <p>Search your favourite track, craft a playlist and save it to your Spotify account</p>
+        </header>
+      
+        <SearchBar />
+
+        <div className='content'>
+          <SearchResults />
+        </div>
+
+      </div>
   );
 }
 
