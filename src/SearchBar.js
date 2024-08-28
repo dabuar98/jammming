e812c9b22e2b644f01fa61f6b0fb41ef56a27b06
Daniel Bulla <dabuar98@gmail.com>
2024-08-27 19:20:40 -0500

    Refactor searchbar component into a function component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,16 +1,19 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import './SearchBar.css'
 
-class SearchBar extends Component {
-    render() {
-        const [search, stateSearch] = useState("");
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            alert(`The form has been submitted with search word ${search}`)
-        }
+function SearchBar () {
+    const [search, stateSearch] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`The form has been submitted with search word ${search}`)
+    }
+
+    return(
         <form onSubmit={handleSubmit} >
             <input value={search} onChange={stateSearch(({ target }) => target.value)}></input>
             <button type="submit">Search</button>
         </form>
-    }
+    )
 }
+
+export default SearchBar;
