5d7c84805a1bbbb1edacf801a3623661bc993b77
Daniel Bulla <dabuar98@gmail.com>
2024-08-28 18:28:08 -0500

    Refactor track element to improve rendering

--- src/Track.js
+++ src/Track.js
@@ -1,30 +1,28 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './Track.css'
 
-class Track extends Component {
-    render() {
-        const { albumCover, trackName, trackAuthor, trackYear } = this.props;
+function Track(props){
 
-        const handleClick = () => {
-            console.log("Test Button clicked");
-        }
+    const { albumCover, trackName, trackAuthor, trackAlbum, trackYear } = props;
 
-        return (
-            <div className="track">
-                <div className="album-cover">
-                    <img src={albumCover} alt="Album Cover" />
-                </div>
-                <div className="track-info">
-                    <p className="track-name">{trackName}</p>
-                    <p className="track-author">{trackAuthor}</p>
-                    <p className="track-year">{trackYear}</p>
-                </div>
-                <div>
-                    <button onClick={handleClick}>Add</button>
-                </div>
-            </div>
-        )
+    const handleClick = () => {
+        console.log("Add button clicked");
     }
+
+    return (
+        <div className="track">
+            <img src={albumCover} alt="Album Cover" />
+            <div className="track-info">
+                <p>{trackName}</p>
+                <p>{trackAlbum}</p>
+                <p>{trackAuthor}</p>
+                <p>{trackYear}</p>
+            </div>
+            <button className="add-button" onClick={handleClick}><FontAwesomeIcon icon={faPlus}/></button>
+            </div>
+    );
 }
 
 export default Track;
