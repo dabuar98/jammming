8edf0ea9b77cb7b297d4a2157495fb1472671d9f
Daniel Bulla <dabuar98@gmail.com>
2024-11-05 19:35:34 -0500

    Refactor Tracklist component

--- src/Tracklist.js
+++ src/Tracklist.js
@@ -2,30 +2,12 @@ import React, { useState } from "react";
 import Track from "./Track";
 import "./Tracklist.css";
 
-function Tracklist() {
-    const [playlistName, setPlaylistName] = useState("");
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`The form has been submitted with playlist name: ${playlistName}`);
-        setPlaylistName("");
-    }
-
+function Tracklist({ tracks }) {
     return (
-        <div className="playlist">
-            <Track  albumCover="../public/logo192.png" 
-                trackName="Sample Track" 
-                trackAuthor="Sample Author" 
-                trackAlbum="Sample Album"
-                trackYear="Sample Year" />
-        
-            <form className="playlist-name" onSubmit={handleSubmit}>
-                <input  value={playlistName} 
-                        placeholder="Name your playlist" 
-                        onChange={({ target }) => setPlaylistName(target.value)}/>
-                <button className="add-playlist">Add</button>
-            </form>
-
+        <div className="tracklist">
+            {tracks.map((track)=>{
+                <Track key={track.id} track={track}/>
+            })}
         </div>
     )
 }
